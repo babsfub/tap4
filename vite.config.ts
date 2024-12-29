139e02f9e7736a5f09a751f30d5f22be806cb5a4
Pumpitlol <151564127+Pumpitlol@users.noreply.github.com>
2024-12-29 23:25:26 +0100

    modified:   package.json 	new file:   vercel.json 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  server: {
-    host: true, // Permet l'accès depuis d'autres machines
-    port: 5173
-  },
+  
   build: {
     target: 'esnext'
   },
